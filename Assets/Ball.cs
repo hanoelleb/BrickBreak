57ccb3ba395d65f0a118505970ba31aa1cbc2bc6
hanoelleb <36749694+hanoelleb@users.noreply.github.com>
2020-07-20 23:17:55 -0700

    first commit

---
+++ Assets/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    float ballForce = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        rb.AddForce(new Vector2(ballForce, ballForce));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    float SPEED = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        processInput();
+    }
+
+    void processInput()
+    {
+        var input = Input.GetAxisRaw("Horizontal");
+        if (input != 0)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * input * SPEED);
+        }
+    }
+}
